c8cfd253f4e364e46674ec0b4ebbd281636f1b48
NomanHameed <nomanbutt8322@gmail.com>
2024-01-08 15:49:43 +0500

    multi domain

---
+++ config/domain.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+  'env_stub' => '.env',
+  'storage_dirs' => [
+    'app' => [
+      'public' => [
+      ],
+    ],
+    'framework' => [
+      'cache' => [
+      ],
+      'testing' => [
+      ],
+      'sessions' => [
+      ],
+      'views' => [
+      ],
+    ],
+    'logs' => [
+    ],
+  ],
+  'domains' => [
+    'tapday.noumanengr.com' => 'tapday.noumanengr.com',
+    'tapday2.noumanengr.com' => 'tapday2.noumanengr.com',
+  ],
+ ];
